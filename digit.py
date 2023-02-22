903d3b549b5496545a446ad8a99580a5d8fd7f66
Sourav Biswal <souravzsn@gmail.com>
2023-02-22 10:49:20 +0530

    python revison

--- digit.py
+++ digit.py
@@ -1,9 +1,8 @@
 while true:
 number=int(input("enter a number"))
-if (number>0 & number<9):
-    print("it is 1 digit number")
-if (number>9 & number<99):
-    print("it is 2 digit number")
-if (number>99 & number<999):
-    print("it is 3 digit number")
-    
+        if (number>0 & number<9):
+            print("it is 1 digit number")
+        if (number>9 & number<99):
+            print("it is 2 digit number")
+        if (number>99 & number<999):
+            print("it is 3 digit number")
---
+++ natural.py
@@ -0,0 +1,3 @@
+n = int(input("enter a numner"))
+for i in range(1,11):
+        print(n,"x",i,"=",i*n)
