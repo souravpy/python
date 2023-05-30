40bea528474fc2886877d829598b9de7907b02aa
Sourav Biswal <souravzsn@gmail.com>
2023-05-30 21:32:18 +0530

    tictac using dict

---
+++ tictac.py
@@ -0,0 +1,7 @@
+board={"11":'', "12":'', "13":'', "21":'', "22":'', "23":'', "31":'', "32":'', "33":''}
+for i in range(9):
+        keyinp=input("enter cell key00")
+        cellinp=input("enter cell valueXO")
+        board[keyinp]=cellinp
+print(board)
+
